b93802eb31f861fa9ebb292e9b365ce4c6f20131
Otavio Braga <otaviobraga@me.com>
2022-11-04 11:29:42 -0300

    Fixed double nested navigation container

--- src/navigation/mainTabs.tsx
+++ src/navigation/mainTabs.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Route} from '@constants';
 import {ShowsStack} from './showsStack';
@@ -9,11 +8,9 @@ const Tab = createBottomTabNavigator();
 
 export const MainTabs = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={Route.Shows} component={ShowsStack} />
-        <Tab.Screen name={Route.Favorites} component={FavoriteShowsStack} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name={Route.Shows} component={ShowsStack} />
+      <Tab.Screen name={Route.Favorites} component={FavoriteShowsStack} />
+    </Tab.Navigator>
   );
 };
